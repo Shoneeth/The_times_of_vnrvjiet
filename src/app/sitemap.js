41012e33887da0d9d9c945710f9123749015b12e
shoneeth <shoneeth.kothakonda@gmail.com>
2023-10-29 11:53:13 +0530

    ...sitemap added...

---
+++ src/app/sitemap.js
@@ -0,0 +1,28 @@
+export default function sitemap() {
+      return [
+        {
+          url: 'https://diurnalis.in',
+          lastModified: new Date(),
+        },
+        {
+          url: 'https://diurnalis.in/about',
+          lastModified: new Date(),
+        },
+        {
+          url: 'https://diurnalis.in/articles',
+          lastModified: new Date(),
+        },
+        {
+            url: 'https://diurnalis.in/events',
+            lastModified: new Date(),
+      },
+      {
+            url: 'https://diurnalis.in/search',
+            lastModified: new Date(),
+          },
+          {
+            url: 'https://diurnalis.in/contact',
+            lastModified: new Date(),
+          },
+      ]
+    }
